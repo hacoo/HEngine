7154a136ba4de8d41a3d57dd75a9cf3585104f60
Henry <hacoo36@gmail.com>
2017-10-24 21:08:21 -0700

    Set up subpass dependencies

--- HEngine/HEngine/Source/VulkanApplication.cpp
+++ HEngine/HEngine/Source/VulkanApplication.cpp
@@ -392,12 +392,26 @@ void VulkanApplication::createRenderPass()
 	subpass.colorAttachmentCount = 1;
 	subpass.pColorAttachments = &colorAttachmentRefInfo;
 
+	VkSubpassDependency dependency = { };
+	// Implicit subpass -- before or after render pass
+	dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+	// Index of target subpass
+	dependency.dstSubpass = 0; 
+	// operation to wait on, stage where it occurs:
+	dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dependency.srcAccessMask = 0;
+	// operations that wait on this:
+	dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+
 	VkRenderPassCreateInfo renderPassInfo = { };
 	renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
 	renderPassInfo.attachmentCount = 1;
 	renderPassInfo.pAttachments = &colorAttachment;
 	renderPassInfo.subpassCount = 1;
 	renderPassInfo.pSubpasses = &subpass;
+	renderPassInfo.dependencyCount = 1;
+	renderPassInfo.pDependencies = &dependency;
 
 	if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass) != VK_SUCCESS)
 	{
